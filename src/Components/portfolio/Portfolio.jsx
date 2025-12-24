d22157c3f205d436cccfa626fae0162b50b5be34
mustafa1912 <mustafa.wahba2404@science.menofia.edu.eg>
2025-12-24 13:22:29 +0200

    mostafa

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -3,27 +3,136 @@ import React from "react";
 import "@fancyapps/fancybox/dist/jquery.fancybox.min.css";
 import "@fancyapps/fancybox/dist/jquery.fancybox.min.js";
 
+// Import all images as modules (this ensures they work in dev and prod)
+
+// Import all images as modules (this ensures they work in dev and prod)
+import ugec1 from "../../assets/img/ugec1.png";
+import ugec2 from "../../assets/img/ugec2.png";
+import ugec3 from "../../assets/img/ugec3.png";
+import ugec4 from "../../assets/img/ugec4.png";
+import jawdaconsultant1 from "../../assets/img/jawdaconsultant1.png"; 
+import jawdaconsultant3 from "../../assets/img/jawdaconsultant3.png";
+import jawdaconsultant4 from "../../assets/img/jawdaconsultant4.png";
+import alMotamizon1 from "../../assets/img/al-motamizon1.png";
+import alMotamizon2 from "../../assets/img/al-motamizon2.png";
+import alMotamizon3 from "../../assets/img/al-motamizon3.png";
+import alMotamizon4 from "../../assets/img/al-motamizon4.png";
+import alMotamizon5 from "../../assets/img/al-motamizon5.png";
+import alMotamizon6 from "../../assets/img/al-motamizon6.png";
+import alMotamizon7 from "../../assets/img/al-motamizon7.png";
+import ghulam1 from "../../assets/img/ghulam1.png";
+import ghulam2 from "../../assets/img/ghulam2.png";
+import ghulam3 from "../../assets/img/ghulam3.png";
+import ghulam4 from "../../assets/img/ghulam4.png";
+import ghulam5 from "../../assets/img/ghulam5.png";
+import ghulam6 from "../../assets/img/ghulam6.png";
+import ghulam7 from "../../assets/img/ghulam7.png"; 
+import ghulam9 from "../../assets/img/ghulam9.png";
+import noorcontrol1 from "../../assets/img/noorcontrol1.png";
+import noorcontrol2 from "../../assets/img/noorcontrol2.png";
+import noorcontrol3 from "../../assets/img/noorcontrol3.png";
+import noorcontrol4 from "../../assets/img/noorcontrol4.png";
+import noorcontrol5 from "../../assets/img/noorcontrol5.png";
+import noorcontrol6 from "../../assets/img/noorcontrol6.png";
+import noorcontrol7 from "../../assets/img/noorcontrol7.png";
+import asiaf1 from "../../assets/img/asiaf1.png";
+import asiaf2 from "../../assets/img/asiaf2.png";
+import asiaf3 from "../../assets/img/asiaf3.png";
+import asiaf4 from "../../assets/img/asiaf4.png";
+import asiaf5 from "../../assets/img/asiaf5.png";
+import kfsHiet1 from "../../assets/img/kfs-hiet1.png";
+import kfsHiet2 from "../../assets/img/kfs-hiet2.png";
+import kfsHiet3 from "../../assets/img/kfs-hiet3.png";
+import kfsHiet4 from "../../assets/img/kfs-hiet4.png";
+import kfsHiet5 from "../../assets/img/kfs-hiet5.png";
+import kfsHiet6 from "../../assets/img/kfs-hiet6.png";
+import kfsHiet7 from "../../assets/img/kfs-hiet7.png";
+import kfsHiet8 from "../../assets/img/kfs-hiet8.png";
+import kfsHiet9 from "../../assets/img/kfs-hiet9.png";
+import kfsHiet10 from "../../assets/img/kfs-hiet10.png";
+import citysquares1 from "../../assets/img/citysquares1.png";
+import citysquares2 from "../../assets/img/citysquares2.png";
+import citysquares3 from "../../assets/img/citysquares3.png";
+import citysquares4 from "../../assets/img/citysquares4.png";
+import citysquares5 from "../../assets/img/citysquares5.png";
+import citysquares6 from "../../assets/img/citysquares6.png";
+import citysquares7 from "../../assets/img/citysquares7.png";
+import osqufia1 from "../../assets/img/osqufia1.png";
+import osqufia2 from "../../assets/img/osqufia2.png";
+import osqufia3 from "../../assets/img/osqufia3.png";
+import osqufia4 from "../../assets/img/osqufia4.png";
+import osqufia5 from "../../assets/img/osqufia5.png";
+import osqufia6 from "../../assets/img/osqufia6.png";
+import osqufia7 from "../../assets/img/osqufia7.png";
+import osqufia8 from "../../assets/img/osqufia8.png";
+import osqufia9 from "../../assets/img/osqufia9.png";
+import osqufia10 from "../../assets/img/osqufia10.png";
+import osqufia11 from "../../assets/img/osqufia11.png";
+import osqufia12 from "../../assets/img/osqufia12.png";
+import osqufia13 from "../../assets/img/osqufia13.png";
+import osqufia14 from "../../assets/img/osqufia14.png";
+import osqufia15 from "../../assets/img/osqufia15.png";
+import osqufia16 from "../../assets/img/osqufia16.png";
+import whitesand1 from "../../assets/img/whitesand1.png";
+import whitesand2 from "../../assets/img/whitesand2.png";
+import whitesand3 from "../../assets/img/whitesand3.png";
+import whitesand4 from "../../assets/img/whitesand4.png";
+import whitesand5 from "../../assets/img/whitesand5.png";
+import us1 from "../../assets/img/us1.png";
+import us2 from "../../assets/img/us2.png";
+import us3 from "../../assets/img/us3.png";
+import us4 from "../../assets/img/us4.png";
+import us5 from "../../assets/img/us5.png";
+import us6 from "../../assets/img/us6.png";
+import us7 from "../../assets/img/us7.png";
+import us8 from "../../assets/img/us8.png";
+import us9 from "../../assets/img/us9.png";
+import cms from "../../assets/img/cms.webp";
+import hcr1 from "../../assets/img/hcr1.webp";
+import naseq8 from "../../assets/img/naseq8.webp";
+import thite1 from "../../assets/img/thite1.webp";
+import thite2 from "../../assets/img/thite2.png";
+import thite3 from "../../assets/img/thite3.png";
+import thite4 from "../../assets/img/thite4.png";
+import thite5 from "../../assets/img/thite5.png";
+import thite6 from "../../assets/img/thite6.png";
+import henkleez7 from "../../assets/img/henkleez7.webp";
+import market1 from "../../assets/img/market1.png";
+import market2 from "../../assets/img/market2.png";
+import market3 from "../../assets/img/market3.png";
+import market4 from "../../assets/img/market4.png";
+import market5 from "../../assets/img/market5.png";
+import market9 from "../../assets/img/market9.webp";
+import dashboed1 from "../../assets/img/dashboed1.webp";
+import dashboed2 from "../../assets/img/dashboed2.webp";
+import dashboed3 from "../../assets/img/dashboed3.webp";
+import dashboed4 from "../../assets/img/dashboed4.webp";
+import dashboed5 from "../../assets/img/dashboed5.webp";
+import dashboed6 from "../../assets/img/dashboed6.webp";
+import dashboed7 from "../../assets/img/dashboed7.webp"; 
+
+
 function Portfolio() {
     const projectCategories = {
         Projects: [
-            { href: 'src/assets/img/ugec1.png', title: 'ugec Project', url: 'https://ugec.souqmsr.com/', date: ' Nov 2024  ' },
-            { href: 'src/assets/img/jawdaconsultant1.png', title: 'jawdaconsultant Project', url: 'https://jawdaconsultant.com', date: ' jan 2024  ' },
-            { href: 'src/assets/img/al-motamizon1.png', title: 'al-motamizon Project', url: 'https://al-motamizon.com/ar', date: ' Dec 2023  ' },
-            { href: 'src/assets/img/ghulam1.png', title: 'ghulam Project', url: 'https://ghulam.najezsoft.com/ar', date: ' Nov 2023  ' },
-            { href: 'src/assets/img/noorcontrol1.png', title: 'noorcontrol Project', url: 'https://noorcontrol.com/', date: ' Nov 2023  ' },
-            { href: 'src/assets/img/asiaf1.png', title: 'asiaf Project', url: 'https://www.asiaf.com.sa/', date: ' oct 2023  ' },
-            { href: 'src/assets/img/kfs-hiet1.png', title: 'kfs-hiet Project', url: 'https://kfs-hiet.edu.eg/', date: ' Aug 2023  ' },
-            { href: 'src/assets/img/citysquares1.png', title: 'citysquares Project', url: 'https://citysquares.com.eg/ar', date: ' jul 2023  ' },
-            { href: 'src/assets/img/osqufia1.png', title: 'osqufia Project', url: '#', date: ' jun 2023  ' },
-            { href: 'src/assets/img/whitesand1.png', title: 'whitesand Project', url: 'https://www.whitesand-eg.com/', date: ' Nov 2021  ' },
-            { href: 'src/assets/img/us1.png', title: 'us Project', url: '#', date: ' may 2023  ' },
-            { href: 'src/assets/img/cms.webp', title: 'CMS Project', url: '#', date: ' Feb 2021  ' },
-            { href: 'src/assets/img/hcr1.webp', title: 'HCR Project', url: '#', date: ' Feb 2021  ' },
-            { href: 'src/assets/img/naseq8.webp', title: 'naseq Project', url: '#', date: ' Feb 2021  ' },
-            { href: 'src/assets/img/thite1.webp', title: 'thite', url: 'https://thiet.edu.eg/ar', date: ' Feb 2021  ' },
-            { href: 'src/assets/img/henkleez7.webp', title: 'henkleez Project', url: 'https://www.henkleez.com/ar', date: ' Feb 2021  ' },
-            { href: 'src/assets/img/market9.webp', title: 'market Project', url: '', date: ' Feb 2020  ' },
-            { href: 'src/assets/img/dashboed7.webp', title: 'dashboed Project', url: '#', date: ' Feb 2020  ' },
+            { href: ugec1, title: 'ugec Project', url: 'https://ugec.souqmsr.com/', date: ' Nov 2024  ' },
+            { href: jawdaconsultant1, title: 'jawdaconsultant Project', url: 'https://jawdaconsultant.com', date: ' jan 2024  ' },
+            { href: alMotamizon1, title: 'al-motamizon Project', url: 'https://al-motamizon.com/ar', date: ' Dec 2023  ' },
+            { href: ghulam1, title: 'ghulam Project', url: 'https://ghulam.najezsoft.com/ar', date: ' Nov 2023  ' },
+            { href: noorcontrol1, title: 'noorcontrol Project', url: 'https://noorcontrol.com/', date: ' Nov 2023  ' },
+            { href: asiaf1, title: 'asiaf Project', url: 'https://www.asiaf.com.sa/', date: ' oct 2023  ' },
+            { href: kfsHiet1, title: 'kfs-hiet Project', url: 'https://kfs-hiet.edu.eg/', date: ' Aug 2023  ' },
+            { href: citysquares1, title: 'citysquares Project', url: 'https://citysquares.com.eg/ar', date: ' jul 2023  ' },
+            { href: osqufia1, title: 'osqufia Project', url: '#', date: ' jun 2023  ' },
+            { href: whitesand1, title: 'whitesand Project', url: 'https://www.whitesand-eg.com/', date: ' Nov 2021  ' },
+            { href: us1, title: 'us Project', url: '#', date: ' may 2023  ' },
+            { href: cms, title: 'CMS Project', url: '#', date: ' Feb 2021  ' },
+            { href: hcr1, title: 'HCR Project', url: '#', date: ' Feb 2021  ' },
+            { href: naseq8, title: 'naseq Project', url: '#', date: ' Feb 2021  ' },
+            { href: thite1, title: 'thite', url: 'https://thiet.edu.eg/ar', date: ' Feb 2021  ' },
+            { href: henkleez7, title: 'henkleez Project', url: 'https://www.henkleez.com/ar', date: ' Feb 2021  ' },
+            { href: market9, title: 'market Project', url: '', date: ' Feb 2020  ' },
+            { href: dashboed7, title: 'dashboed Project', url: '#', date: ' Feb 2020  ' },
         ],
         dashboed: [
             { href: 'src/assets/img/dashboed1.png', title: 'dashboed Project' },
@@ -157,9 +266,9 @@ function Portfolio() {
             { href: 'src/assets/img/whitesand5.png', title: 'whitesand Project', date: ' Nov 2023  ' },
         ],
 
-
     };
 
+    // The rest of your component remains the same
     // Reusable render function for project grids
     const renderGrid = (category) =>
         projectCategories[category].map((item, index) => (
---
+++ src/assets/imgs.js
@@ -0,0 +1,75 @@
+/* ================= Ugec ================= */
+export { default as ugec1 } from "./img/ugec/ugec1.png";
+export { default as ugec2 } from "./img/ugec/ugec2.png";
+export { default as ugec3 } from "./img/ugec/ugec3.png";
+export { default as ugec4 } from "./img/ugec/ugec4.png";
+
+/* ============ Jawda Consultant ============ */
+export { default as jawdaconsultant1 } from "./img/jawdaconsultant1.png";
+export { default as jawdaconsultant2 } from "./img/jawdaconsultant2.png";
+export { default as jawdaconsultant3 } from "./img/jawdaconsultant3.png";
+export { default as jawdaconsultant4 } from "./img/jawdaconsultant4.png";
+
+/* ============ Al Motamizon ============ */
+export { default as al_motamizon1 } from "./img/al-motamizon/al-motamizon1.png";
+export { default as al_motamizon2 } from "./img/al-motamizon/al-motamizon2.png";
+export { default as al_motamizon3 } from "./img/al-motamizon/al-motamizon3.png";
+export { default as al_motamizon4 } from "./img/al-motamizon/al-motamizon4.png";
+export { default as al_motamizon5 } from "./img/al-motamizon/al-motamizon5.png";
+export { default as al_motamizon6 } from "./img/al-motamizon/al-motamizon6.png";
+export { default as al_motamizon7 } from "./img/al-motamizon/al-motamizon7.png";
+
+/* ================= Asiaf ================= */
+export { default as asiaf1 } from "./img/asiaf/asiaf1.png";
+export { default as asiaf2 } from "./img/asiaf/asiaf2.png";
+export { default as asiaf3 } from "./img/asiaf/asiaf3.png";
+export { default as asiaf4 } from "./img/asiaf/asiaf4.png";
+export { default as asiaf5 } from "./img/asiaf/asiaf5.png";
+
+/* ================= Dashboed ================= */
+export { default as dashboed1 } from "./img/dashboed/dashboed1.webp";
+export { default as dashboed2 } from "./img/dashboed/dashboed2.webp";
+export { default as dashboed3 } from "./img/dashboed/dashboed3.webp";
+export { default as dashboed4 } from "./img/dashboed/dashboed4.webp";
+export { default as dashboed5 } from "./img/dashboed/dashboed5.webp";
+export { default as dashboed6 } from "./img/dashboed/dashboed6.webp";
+export { default as dashboed7 } from "./img/dashboed/dashboed7.webp";
+export { default as dashboed8 } from "./img/dashboed/dashboed8.webp";
+export { default as dashboed9 } from "./img/dashboed/dashboed9.webp";
+
+/* ================= Ghulam ================= */
+export { default as ghulam1 } from "./img/ghulam/ghulam1.png";
+export { default as ghulam2 } from "./img/ghulam/ghulam2.png";
+export { default as ghulam3 } from "./img/ghulam/ghulam3.png";
+export { default as ghulam4 } from "./img/ghulam/ghulam4.png";
+export { default as ghulam5 } from "./img/ghulam/ghulam5.png";
+export { default as ghulam6 } from "./img/ghulam/ghulam6.png";
+export { default as ghulam7 } from "./img/ghulam/ghulam7.png";
+export { default as ghulam8 } from "./img/ghulam/ghulam8.png";
+export { default as ghulam9 } from "./img/ghulam/ghulam9.png";
+
+/* ================= CitySquares ================= */
+export { default as citysquares1 } from "./img/citysquares/citysquares1.png";
+export { default as citysquares2 } from "./img/citysquares/citysquares2.png";
+export { default as citysquares3 } from "./img/citysquares/citysquares3.png";
+export { default as citysquares4 } from "./img/citysquares/citysquares4.png";
+export { default as citysquares5 } from "./img/citysquares/citysquares5.png";
+export { default as citysquares6 } from "./img/citysquares/citysquares6.png";
+export { default as citysquares7 } from "./img/citysquares/citysquares7.png";
+
+/* ================= CMS / HCR / Naseq ================= */
+export { default as cms } from "./img/cms.webp";
+
+export { default as hcr1 } from "./img/hcr/hcr1.png";
+export { default as hcr2 } from "./img/hcr/hcr2.png";
+export { default as hcr3 } from "./img/hcr/hcr3.png";
+export { default as hcr4 } from "./img/hcr/hcr4.png";
+
+export { default as naseq1 } from "./img/naseq/naseq1.webp";
+export { default as naseq2 } from "./img/naseq/naseq2.webp";
+export { default as naseq3 } from "./img/naseq/naseq3.webp";
+export { default as naseq4 } from "./img/naseq/naseq4.webp";
+export { default as naseq5 } from "./img/naseq/naseq5.webp";
+export { default as naseq6 } from "./img/naseq/naseq6.webp";
+export { default as naseq7 } from "./img/naseq/naseq7.webp";
+export { default as naseq8 } from "./img/naseq/naseq8.webp";
