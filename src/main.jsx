38029ae6665634a1efcfb72bf5379fb97ba6e962
mustafa1912 <mustafa.wahba2404@science.menofia.edu.eg>
2025-12-24 14:17:19 +0200

    mostafa

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+
 // fontawesome
 import './assets/style/fontawesome/fontawesome/css/all.min.css';
 // bootstrap
@@ -16,7 +17,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </BrowserRouter>
   </StrictMode>,
 )
